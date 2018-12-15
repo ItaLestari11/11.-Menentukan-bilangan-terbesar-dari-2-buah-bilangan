1d7bad982333a686e87cfddd619e7e8750e08b93
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 16:11:56 +0700

    Add files via upload

---
+++ Bilangan terbesar dari 2 bilangan.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int a,b;
+
+cout<<"Menentukan bilangan terbesar dari 2 buah bilangan"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"Masukan bilangan ke 1"<<endl;
+cin>>a;
+cout<<"Masukan bilangan ke 2"<<endl;
+cin>>b;
+
+if(a>b)
+   cout<<"Maka bilangan terbesar adalah"<<a<<endl;
+   else
+    cout<<"Maka bilangan terbesar adalah"<<b;
+   return 0;
+}
